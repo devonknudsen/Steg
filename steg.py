e18af905d2e617e7cc417c1263db3e07f60093e8
Devon Knudsen <kingdevon12@gmail.com>
2019-10-24 11:40:57 -0500

    Worked to implement bit method. Storing works, but retrieving does not

--- steg.py
+++ steg.py
@@ -3,30 +3,59 @@ import sys
 s = [0x0, 0xff, 0x0, 0x0, 0xff, 0x0]
 SENTINEL = bytearray(s)
 
-def store(method, w, h, o, i):
+def store(method, wrapper, h, offset, interval):
     if(method == 'B'):
         x = 0
         while(x < len(h)):
-            w[o] = h[x]
-            o += i
+            wrapper[offset] = h[x]
+            offset += interval
             x += 1
         
         x = 0
         while (x < len(SENTINEL)):
-            w[o] = SENTINEL[x]
-            o += i
+            wrapper[offset] = SENTINEL[x]
+            offset += interval
             x += 1
+    
+    elif(method == 'b'):
+        j = 0
+        while(j < len(h)):
+            for k in range(0, 8):
+                
+                # 254 is decimal for binary 11111110
+                wrapper[offset] &= 254
+                
+                # 128 is decimal for binary 10000000
+                # if hidden bit is a 1, add it to wrapper in LSB place
+                wrapper[offset] |= ((h[j] & 128) >> 7)
+                
+                # move to the next bit in the current hiddden byte
+                h[j] = (h[j] << 1) & (2 ** 8 - 1)
+                offset += interval
             
-        return w
+            j += 1
+        
+        j = 0
+        while(j < len(SENTINEL)):
+            for k in range(0, 8):
+                wrapper[offset] &= 254
+                wrapper[offset] |= ((SENTINEL[j] & 128) >> 7)
+                SENTINEL[j] = (SENTINEL[j] << 1) & (2 ** 8 - 1)
+                offset += interval
+            
+            j += 1
+        
+    return wrapper
+                
 
-def retrieve(method, w, o, i):
+def retrieve(method, wrapper, offset, interval):
     h = bytearray()
     if(method == 'B'):
-        while(o < len(w)):
-            currByte = w[o]
+        while(offset < len(wrapper)):
+            currByte = wrapper[offset]
             if(currByte == SENTINEL[0]):
-                
                 sentFinder = bytearray()
+                
                 for sByte in SENTINEL:
                     
                     if(currByte != sByte):
@@ -36,14 +65,59 @@ def retrieve(method, w, o, i):
                         break
                     
                     sentFinder.append(currByte)
-                    o += i
-                    currByte = w[o]
+                    offset += interval
+                    currByte = wrapper[offset]
                     
                 if(sentFinder == SENTINEL):
                     return h
             else:
                 h.append(currByte)
-                o += i
+                offset += interval
+    
+    elif(method == 'b'):
+        while(offset < len(wrapper)):
+            b = 0
+            for j in range(0, 8):
+                # if the LSB of wrapper byte is a 1 add, if not, keep 0
+                b |= (wrapper[offset] & 1)
+                if(j < 7):
+                    # move b over to the left one bit
+                    b = (b << 1) & (2 ** 8 - 1)
+                    offset += interval
+            
+            if(b == SENTINEL[0]):
+                print('{} == {}'.format(b, SENTINEL[0]))
+                # print(bin(SENTINEL[0]))
+                sF = bytearray()
+                sF.append(b)
+                offset += interval
+                
+                for m in range(1, len(SENTINEL)):
+                    b = 0
+                    for k in range(0, 8):
+                        # if the LSB of wrapper byte is a 1 add, if not, keep 0
+                        b |= (wrapper[offset] & 1)
+                        if(k < 7):
+                            # move b over to the left one bit
+                            b = (b << 1) & (2 ** 8 - 1)
+                            offset += interval
+                    
+                    if(b != SENTINEL[m]):
+                        h.append(b)
+                        for byte in sF:
+                            h.append(byte)
+                        
+                        break
+                    else:
+                        sF.append(b)
+                        offset += interval
+                
+                if(sF == SENTINEL):
+                    return h
+            else:
+                h.append(b)
+                offset += interval
+                        
 
 # MAIN
 sR = sys.argv[1][1]
@@ -58,8 +132,8 @@ if(sys.argv[4][2:] != None):
     interval = int(sys.argv[4][2:])
 
 with open(sys.argv[5][2:], 'rb') as wImg:
-    w = wImg.read()
-    wBytes = bytearray(w)
+    wrapper = wImg.read()
+    wBytes = bytearray(wrapper)
 
 if(sR == 's'):
     with open(sys.argv[6][2:], 'rb') as hImg:
